e4d0b6080f2f49869fe47e738c3336b6925bbca6
intensity <intensity.web@gmail.com>
2015-01-25 13:41:00 +0200

    add some functionality into MemcachedMessageCatalogue

--- Controller/TranslationController.php
+++ Controller/TranslationController.php
@@ -2,34 +2,24 @@
 
 namespace Sleepness\UberTranslationBundle\Controller;
 
+use Sleepness\UberTranslationBundle\Translation\MemcachedMessageCatalogue;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class TranslationController extends Controller
 {
     public function indexAction()
     {
+        $messageCatalogue = new MemcachedMessageCatalogue();
         $mem = $this->get('uber.memcached');
         $locales = array('uk', 'en');
-        $preparedTranslations = array();
         foreach ($locales as $key => $locale) {
             $translations = $mem->getItem($locale);
-            foreach ($translations as $domain => $messages) {
-                foreach ($messages as $key => $message) {
-                    $preparedTranslations[] = array(
-                        'domain' => $domain,
-                        'key' => $key,
-                        'message' => array(
-                            'messageText' => $message,
-                            'locale' => $locale,
-                        ),
-                    );
-                }
-            }
+            $messageCatalogue->add($locale, $translations);
         }
-
+        $messages = $messageCatalogue->getAll();
 
         return $this->render('SleepnessUberTranslationBundle:Translation:index.html.twig', array(
-            'messages' => $preparedTranslations,
+            'messages' => $messages,
         ));
     }
 }
--- Translation/MemcachedMessageCatalogue.php
+++ Translation/MemcachedMessageCatalogue.php
@@ -2,9 +2,42 @@
 
 namespace Sleepness\UberTranslationBundle\Translation;
 
-use Symfony\Component\Translation\MessageCatalogue as BaseMessageCatalogue;
-
-class MemcachedMessageCatalogue extends BaseMessageCatalogue
+/**
+ * Prepare messages for output
+ */
+class MemcachedMessageCatalogue
 {
+    private $preparedTranslations = array();
 
+    /**
+     * Add new translations of certain locale to be prepared for output
+     *
+     * @param $locale
+     * @param $translations
+     */
+    public function add($locale, $translations)
+    {
+        foreach ($translations as $domain => $messages) {
+            foreach ($messages as $key => $message) {
+                $this->preparedTranslations[] = array(
+                    'domain' => $domain,
+                    'key' => $key,
+                    'message' => array(
+                        'messageText' => $message,
+                        'locale' => $locale,
+                    ),
+                );
+            }
+        }
+    }
+
+    /**
+     * Get array of translations prepared for output
+     *
+     * @return array
+     */
+    public function getAll()
+    {
+        return $this->preparedTranslations;
+    }
 }
