fb4cec6e1c16a2b892ceef300ef77db1017d1bb3
alexandrzhulev <alexandrzhulev@gmail.com>
2015-02-02 17:28:43 +0200

    fixed usage of MemcachedMessageCatalogueTest

--- Tests/Translation/MemcachedMessageCatalogueTest.php
+++ Tests/Translation/MemcachedMessageCatalogueTest.php
@@ -2,15 +2,91 @@
 
 namespace Sleepness\UberTranslationBundle\Tests\Translation;
 
+require_once dirname(__DIR__).'/../../../../app/AppKernel.php';
+
 use Sleepness\UberTranslationBundle\Translation\MemcachedMessageCatalogue;
 
 
 class MemcachedMessageCatalogueTest extends \PHPUnit_Framework_TestCase
 {
-    public function testAdd()
+    /**
+     * @var \AppKernel
+     */
+    protected $kernel;
+
+    /**
+     * @var Symfony\Component\DependencyInjection\Container
+     */
+    protected $container;
+
+    /*
+     * @var Sleepness\UberTranslationBundle\Translation\MemcachedMessageCatalogue;
+     */
+    protected $messageCatalogue;
+
+    /*
+     * @var Sleepness\UberTranslationBundle\Cache\UberMemcached;
+     */
+    protected $uberMemcached;
+
+
+    public function testBuildByLocale()
     {
-        $messageCatalogue = new MemcachedMessageCatalogue();
-        $translations = [
+        $messageCatalogue = $this->messageCatalogue;
+        $preparedTranslations = $messageCatalogue->buildByLocale('en');
+
+        $this->assertEquals('key.hello', $preparedTranslations[0]['keyYml']);
+        $this->assertEquals('value.Hello', $preparedTranslations[0]['messages'][0]['messageText']);
+        $this->assertEquals('en', $preparedTranslations[0]['messages'][0]['locale']);
+    }
+
+    public function testBuildByDomain()
+    {
+        $messageCatalogue = $this->messageCatalogue;
+        $preparedTranslations = $messageCatalogue->buildByDomain('messages');
+
+        $this->assertEquals('key.hello', $preparedTranslations[0]['keyYml']);
+        $this->assertEquals('value.Hello', $preparedTranslations[0]['messages'][0]['messageText']);
+        $this->assertArrayNotHasKey('validators', $preparedTranslations);
+    }
+
+    public function testBuildByKey()
+    {
+        $messageCatalogue = $this->messageCatalogue;
+        $preparedTranslations = $messageCatalogue->buildByKey('key.not.blank');
+
+        $this->assertEquals('key.not.blank', $preparedTranslations[0]['keyYml']);
+        $this->assertEquals('value.NotBlank', $preparedTranslations[0]['messages'][0]['messageText']);
+        $this->assertArrayNotHasKey('messages', $preparedTranslations);
+    }
+
+    public function testBuildByText()
+    {
+        $messageCatalogue = $this->messageCatalogue;
+        $preparedTranslations = $messageCatalogue->buildByText('value.MaxLength');
+
+        $this->assertEquals('key.max.length', $preparedTranslations[0]['keyYml']);
+        $this->assertEquals('value.MaxLength', $preparedTranslations[0]['messages'][0]['messageText']);
+        $this->assertArrayNotHasKey('messages', $preparedTranslations);
+    }
+
+    public function testGetAll()
+    {
+        $messageCatalogue = $this->messageCatalogue;
+        $preparedTranslations = $messageCatalogue->getAll();
+
+        $this->assertEquals('key.max.length', $preparedTranslations[3]['keyYml']);
+        $this->assertEquals('value.MaxLength', $preparedTranslations[3]['messages'][0]['messageText']);
+    }
+
+    public function setUp()
+    {
+        $this->kernel = new \AppKernel('test', true);
+        $this->kernel->boot();
+        $this->container = $this->kernel->getContainer();
+
+        $this->uberMemcached = $this->container->get('uber.memcached');
+        $values = [
             'messages' => [
                 'key.hello' => 'value.Hello',
                 'key.foo' => 'value.Foo',
@@ -20,13 +96,7 @@ class MemcachedMessageCatalogueTest extends \PHPUnit_Framework_TestCase
                  'key.max.length' => 'value.MaxLength',
             ],
         ];
-
-        $messageCatalogue->add('en', $translations);
-        $preparedTransl = $messageCatalogue->getAll();
-
-        $this->assertEquals('messages', $preparedTransl[0]['domain']);
-        $this->assertEquals('key.hello', $preparedTransl[0]['keyYml']);
-        $this->assertEquals('value.Hello', $preparedTransl[0]['messages'][0]['messageText']);
-        $this->assertEquals('en', $preparedTransl[0]['messages'][0]['locale']);
+        $this->uberMemcached->addItem('en', $values);
+        $this->messageCatalogue = $this->container->get('memcached.message.catalogue');
     }
 }
