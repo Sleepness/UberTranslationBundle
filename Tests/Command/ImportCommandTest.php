6aa80912cf21a92ae810581ebc544ba991c1def5
NovikovViktor <viktor.novikov95@gmail.com>
2015-03-31 10:07:46 +0300

    place boot logic into setUp() method in tests

--- Tests/Command/ImportCommandTest.php
+++ Tests/Command/ImportCommandTest.php
@@ -11,24 +11,28 @@ use Sleepness\UberTranslationBundle\Command\ImportCommand;
  * Test ImportCommand executing cases
  *
  * @author Viktor Novikov <viktor.novikov95@gmail.com>
- * @author Alexandr Zhulev
+ * @author Alexandr Zhulev <alexandrzhulev@gmail.com>
  */
 class ImportCommandTest extends KernelTestCase
 {
+    /**
+     * @var \Symfony\Component\Console\Tester\CommandTester;
+     */
+    private $commandTester;
+
     /**
      * Test command success execution
      */
     public function testSuccessExecute()
     {
-        $commandTester = $this->bootCommandTestKernel();
-        $commandTester->execute(
+        $this->commandTester->execute(
             array(
                 'locales' => 'en,uk',
                 'bundle' => 'TestBundle',
             )
         );
-        $this->assertTrue(is_string($commandTester->getDisplay()));
-        $this->assertEquals("\033[37;42m Translations from TestBundle imported successfully! \033[0m", trim($commandTester->getDisplay()));
+        $this->assertTrue(is_string($this->commandTester->getDisplay()));
+        $this->assertEquals("\033[37;42m Translations from TestBundle imported successfully! \033[0m", trim($this->commandTester->getDisplay()));
     }
 
     /**
@@ -36,31 +40,26 @@ class ImportCommandTest extends KernelTestCase
      */
     public function testFailureExecute()
     {
-        $commandTester = $this->bootCommandTestKernel();
-        $commandTester->execute(
+        $this->commandTester->execute(
             array(
                 'locales' => 'eqwewqeqwe12341241',
                 'bundle' => 'TestBundle',
             )
         );
-        $this->assertTrue(is_string($commandTester->getDisplay()));
-        $this->assertEquals("\033[37;43m Make sure you define all locales properly \033[0m", trim($commandTester->getDisplay()));
+        $this->assertTrue(is_string($this->commandTester->getDisplay()));
+        $this->assertEquals("\033[37;43m Make sure you define all locales properly \033[0m", trim($this->commandTester->getDisplay()));
     }
 
     /**
      * Boot application for testing import command
-     *
-     * @return CommandTester
      */
-    private function bootCommandTestKernel()
+    public function setUp()
     {
         $kernel = $this->createKernel();
         $kernel->boot();
         $application = new Application($kernel);
         $application->add(new ImportCommand());
         $command = $application->find('uber:translations:import');
-        $commandTester = new CommandTester($command);
-
-        return $commandTester;
+        $this->commandTester = new CommandTester($command);
     }
 }
--- Tests/Command/PurgeCommandTest.php
+++ Tests/Command/PurgeCommandTest.php
@@ -11,35 +11,35 @@ use Sleepness\UberTranslationBundle\Command\PurgeCommand;
  * Test memcached purging command
  *
  * @author Viktor Novikov <viktor.novikov95@gmail.com>
- * @author Alexandr Zhulev
+ * @author Alexandr Zhulev <alexandrzhulev@gmail.com>
  */
 class PurgeCommandTest extends KernelTestCase
 {
+    /**
+     * @var \Symfony\Component\Console\Tester\CommandTester;
+     */
+    private $commandTester;
+
     /**
      * Test purge command execution
      */
     public function testExecute()
     {
-        $commandTester = $this->bootCommandTestKernel();
-        $commandTester->execute(array());
-        $this->assertTrue(is_string($commandTester->getDisplay()));
-        $this->assertEquals("\033[37;42m All translations deleted from Memcache! \033[0m", trim($commandTester->getDisplay()));
+        $this->commandTester->execute(array());
+        $this->assertTrue(is_string($this->commandTester->getDisplay()));
+        $this->assertEquals("\033[37;42m All translations deleted from Memcache! \033[0m", trim($this->commandTester->getDisplay()));
     }
 
     /**
-     * Boot application for testing purge command
-     *
-     * @return CommandTester
+     * Boot application for testing import command
      */
-    private function bootCommandTestKernel()
+    public function setUp()
     {
         $kernel = $this->createKernel();
         $kernel->boot();
         $application = new Application($kernel);
         $application->add(new PurgeCommand());
         $command = $application->find('uber:translations:purge');
-        $commandTester = new CommandTester($command);
-
-        return $commandTester;
+        $this->commandTester = new CommandTester($command);
     }
-} 
+}
