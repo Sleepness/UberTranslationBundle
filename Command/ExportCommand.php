20ddade6ffb9de4142ee977e19392c3ffd1648ef
NovikovViktor <viktor.novikov95@gmail.com>
2015-04-24 12:32:47 +0300

    Add improvements to ExportCommand

--- Command/ExportCommand.php
+++ Command/ExportCommand.php
@@ -48,46 +48,43 @@ Command example:
         $bundlePath = $this->getContainer()->get('kernel')->getBundle($bundleName)->getPath();
         $translationDirPath = $bundlePath . '/Resources/translations/';
         $uberMemcached = $this->getContainer()->get('uber.memcached');
-        $locales = $uberMemcached->getAllKeys();
-        if ($locales) {
-            $numberOfLocales = 0;
-            foreach ($locales as $locale) {
-                if (preg_match('/^[a-z]{2}$/', $locale) || preg_match('/^[a-z]{2}_[A-Z]{2}$/', $locale)) {
-                    if (!file_exists($translationDirPath)) {
-                        mkdir($translationDirPath, 0777, true);
-                    }
-                    $numberOfLocales++;
-                    $memcacheMessages = $uberMemcached->getItem($locale);
-                    foreach ($memcacheMessages as $domain => $messagesArray) {
-                        $yamlArr = array();
-                        $indent = 0;
-                        foreach ($messagesArray as $key => $value) {
-                            $delimArray = explode('.', $key);
-                            $indent = count($delimArray);
-                            array_push($delimArray, $value);
-                            $expArray = $this->expand($delimArray);
-                            foreach ($expArray as $expArrayKey => $expArrayVal) {
-                                if (array_key_exists($expArrayKey, $yamlArr)) {
-                                    $yamlArr[$expArrayKey] = array_replace_recursive($yamlArr[$expArrayKey], $expArrayVal);
-                                } else {
-                                    $yamlArr[$expArrayKey] = $expArrayVal;
-                                }
+        $locales = $this->getContainer()->getParameter('sleepness_uber_translation.supported_locales');
+        $response = "\033[37;43m No translations in Memcache! \033[0m";
+        $numberOfLocales = 0;
+        foreach ($locales as $locale) {
+            if (preg_match('/^[a-z]{2}$/', $locale) || preg_match('/^[a-z]{2}_[A-Z]{2}$/', $locale)) {
+                if (!file_exists($translationDirPath)) {
+                    mkdir($translationDirPath, 0777, true);
+                }
+                $numberOfLocales++;
+                $memcacheMessages = $uberMemcached->getItem($locale);
+                foreach ($memcacheMessages as $domain => $messagesArray) {
+                    $yamlArr = array();
+                    $indent = 0;
+                    foreach ($messagesArray as $key => $value) {
+                        $delimArray = explode('.', $key);
+                        $indent = count($delimArray);
+                        array_push($delimArray, $value);
+                        $expArray = $this->expand($delimArray);
+                        foreach ($expArray as $expArrayKey => $expArrayVal) {
+                            if (array_key_exists($expArrayKey, $yamlArr)) {
+                                $yamlArr[$expArrayKey] = array_replace_recursive($yamlArr[$expArrayKey], $expArrayVal);
+                            } else {
+                                $yamlArr[$expArrayKey] = $expArrayVal;
                             }
                         }
-                        $dumper = new Dumper();
-                        $yaml = $dumper->dump($yamlArr, $indent);
-                        file_put_contents($translationDirPath . '/' . $domain . '.' . $locale . '.yml', $yaml);
                     }
+                    $dumper = new Dumper();
+                    $yaml = $dumper->dump($yamlArr, $indent);
+                    file_put_contents($translationDirPath . '/' . $domain . '.' . $locale . '.yml', $yaml);
                 }
             }
-            if ($numberOfLocales) {
-                $output->writeln("\033[37;42m Translations exported successfully into " . $bundleName . "/Resources/translations/ ! \033[0m");
-            } else {
-                $output->writeln("\033[37;43m No translations in Memcache! \033[0m");
-            }
-        } else {
-            $output->writeln("\033[37;43m Memcache is empty! \033[0m");
         }
+        if (!empty($numberOfLocales)) {
+            $response = "\033[37;42m Translations exported successfully into " . $bundleName . "/Resources/translations/ ! \033[0m";
+        }
+
+        $output->writeln($response);
     }
 
     /**
