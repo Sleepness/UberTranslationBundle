fc45086f83fb6c88b09b5f09ed11a6a508ddb2c1
Novikov Viktor <viktor.novikov95@gmail.com>
2015-07-27 17:19:01 +0300

    Add more method to Redis cache wrapper and add some configuration

--- Cache/UberRedis.php
+++ Cache/UberRedis.php
@@ -24,11 +24,65 @@ class UberRedis implements ResourceInterface
     }
 
     /**
+     * Set the connection with Redis server
+     *
+     * @param string $host
+     * @param int $port
+     * @return bool TRUE on success or FALSE failure.
+     */
+    public function setConnection($host, $port)
+    {
+        $this->getRedis()->connect($host, $port);
+    }
+
+    /**
      * @return Redis
      */
     public function getRedis()
     {
         return $this->redis;
+    }
+
+    /**
+     * Add the item into Redis. If value with given keys exist,
+     * it will only replace the value.
+     *
+     * @param $key
+     * @param $value
+     * @param null $expiration
+     * @return bool
+     */
+    public function addItem($key, $value, $expiration = null)
+    {
+        if ($expiration === null) {
+            $expiration = 60 * 60 * 24 * 30; // default expires after 30 days
+        }
+        $encoded_value = json_encode($value);
+
+        return $this->getRedis()->set($key, $encoded_value, $expiration);
+    }
+
+    /**
+     * Get item from Redis
+     *
+     * @param $key
+     * @return mixed
+     */
+    public function getItem($key)
+    {
+        $response = json_decode($this->getRedis()->get($key));
+
+        return $response;
+    }
+
+    /**
+     * Get all keys(locales identifiers) stored on all the Redis storage
+     *
+     * @return array - locales identifiers
+     */
+    public function getAllKeys()
+    {
+        return $this->getRedis()->keys('*');
     }
 
     /**
