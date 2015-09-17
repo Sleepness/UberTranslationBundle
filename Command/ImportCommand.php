044a3c6392e739cad6839272e8206d035fd7d081
Novikov Viktor <viktorn@default-value.com>
2015-09-17 21:46:03 +0300

    Added more common regular expression to validate defined locales

--- Command/ImportCommand.php
+++ Command/ImportCommand.php
@@ -45,15 +45,18 @@ Command example:
     {
         $locales = $this->getContainer()->getParameter('sleepness_uber_translation.supported_locales'); // prepare locales from parameters
         $bundle = $this->getContainer()->get('kernel')->getBundle($input->getArgument('bundle')); // get bundle props
+
         if (!is_dir($bundle->getPath() . '/Resources/translations')) {
             $output->writeln("\033[37;43m There is no folder with translations in " . $input->getArgument('bundle') . " \033[0m");
             return;
         }
+
         $loader = $this->getContainer()->get('translation.loader'); // get translator loader
         $uberMemcached = $this->getContainer()->get('uber.memcached'); // get uber memcached
         $catalogues = array(); // prepare array for catalogues
+
         foreach ($locales as $locale) { // run through locales
-            if (!preg_match('/^[a-z]{2}$/', $locale) && !preg_match('/^[a-z]{2}_[A-Z]{2}$/', $locale)) {
+            if (!preg_match('/^[a-z]{2}_[a-zA-Z]{2}$|[a-z]{2}/', $locale)) {
                 $output->writeln("\033[37;43m Make sure you define all locales properly \033[0m   \n");
                 return;
             }
@@ -61,9 +64,11 @@ Command example:
             $loader->loadMessages($bundle->getPath() . '/Resources/translations', $currentCatalogue); // load messages from catalogue
             $catalogues[$locale] = $currentCatalogue; // pass MessageCatalogue instance into $catalogues array
         }
+
         foreach ($catalogues as $locale => $catalogue) { // run over all catalogues
             $catalogueMessages = $catalogue->all(); // get messages from current catalogue
             $memcacheMessages = $uberMemcached->getItem($locale); // get existing messages from the memcache by locale
+
             if (!$memcacheMessages) { // if for now no messages in memcache
                 $uberMemcached->addItem($locale, $catalogueMessages); // just upload them
             } else {
@@ -84,6 +89,7 @@ Command example:
                 $uberMemcached->addItem($locale, $mergedMessages); // set merged messages to memcache
             }
         }
+
         $output->writeln("\033[37;42m Translations from " . $input->getArgument('bundle') . " imported successfully! \033[0m");
     }
 }
